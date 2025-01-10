00c1e699bd5b2c0caf8dfe8eab18a44d6d4af9e0
saiful-islam <saifulislam109710@gmail.com>
2025-01-10 22:36:25 +0600

    add parallax menu cover

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 
+import menuImg from '../../assets/menu/banner3.jpg'
+import Cover from '../shared/Cover/Cover'
+import PopularMenu from '../Home/PopularMenu/PopularMenu'
+
+
 const Menu = () => {
   return (
     <div>
         <Helmet>
             <title>Menu | Bistro Boss</title>
         </Helmet>
+        <Cover img={menuImg} title='Our Menu'></Cover>
+        <PopularMenu></PopularMenu>
+        <Cover img={menuImg} title='Our Menu'></Cover>
+        <PopularMenu></PopularMenu>
+        <Cover img={menuImg} title='Our Menu'></Cover>
+        <PopularMenu></PopularMenu>
     </div>
   )
 }
