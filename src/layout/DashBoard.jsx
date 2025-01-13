2293be88d03ac462c1789ab0fb0c4acc44e39e60
saiful-islam <saifulislam109710@gmail.com>
2025-01-14 00:49:13 +0600

    cart page complte

--- src/layout/DashBoard.jsx
+++ src/layout/DashBoard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { FaCalendar, FaComment, FaHome, FaList, FaShoppingCart } from 'react-icons/fa'
+import { FaCalendar, FaComment, FaHome, FaList, FaSearch, FaShoppingCart } from 'react-icons/fa'
 import { NavLink, Outlet } from 'react-router-dom'
+import useCart from '../hooks/useCart'
 
 const DashBoard = () => {
+
+    const [cart] = useCart()
   return (
     <div className='flex'>
         {/* dashboard sidebar */}
@@ -10,14 +13,18 @@ const DashBoard = () => {
             <ul className='menu p-4 space-y-2'>
                  <li><NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink></li>
                  <li><NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar> Reservation</NavLink></li>
-                <li><NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
+                <li><NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart ({cart.length})</NavLink></li>
                 <li><NavLink to='/dashboard/review'><FaComment></FaComment> Add a Review</NavLink></li>
                 <li><NavLink to='/dashboard/bookings'><FaList></FaList> My Bookings</NavLink></li>
+                <div className='divider'></div>
+                <li><NavLink to='/'><FaHome></FaHome> Home</NavLink></li>
+                <li><NavLink to='/order/salad'><FaSearch></FaSearch> Order</NavLink></li>
+
                 
             </ul>
         </div>
         {/* dashboard*/}
-        <div className='flex-1'>
+        <div className='flex-1 p-8'>
             <Outlet></Outlet>
         </div>
     </div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from "react";
+import useCart from "../../../hooks/useCart";
+import { FaTrash, FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
+  const [cart,refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
-        <h2 className='text-3xl font-bold'>My Cart</h2>
+      <div className="flex justify-evenly my-8">
+        <h2 className="text-3xl font-bold">Items:{cart.length}</h2>
+        <h2 className="text-3xl font-bold">Total Price:{totalPrice}</h2>
+        <button className="btn btn-warning">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, idx) => (
+              <tr key={item._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td>${item.price}</td>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost "
+                  >
+                    <FaTrashAlt className="text-red-500"></FaTrashAlt>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
